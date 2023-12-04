ceb9ca7908ec148aec6c8d19eef765aac4288336
DaAlbrecht <dadidi42@gmail.com>
2023-12-04 22:08:35 +0100

    part_02

--- 2023/day_03/src/main.rs
+++ 2023/day_03/src/main.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct MachinePart {
     serial_number: u32,
     x_start: usize,
@@ -15,6 +15,7 @@ struct Symbol {
 fn main() {
     let input = std::fs::read_to_string("puzzle").unwrap();
     println!("part 01: {}", part_01(&input));
+    println!("part 02: {}", part_02(&input));
 }
 
 fn part_01(input: &str) -> u32 {
@@ -24,24 +25,55 @@ fn part_01(input: &str) -> u32 {
     let parts = input
         .lines()
         .enumerate()
-        .map(|(i, line)| parse_digits(i, line))
-        .flatten()
+        .flat_map(|(i, line)| parse_digits(i, line))
         .collect::<Vec<_>>();
 
     let symbols = input
         .lines()
         .enumerate()
         .fold(vec![], |mut acc: Vec<Symbol>, (i, line)| {
-            acc.append(&mut parse_symbols(i, line, available_symbols.clone()));
+            acc.append(&mut parse_symbols(i, line, &available_symbols));
             acc
         });
 
     parts
         .iter()
-        .filter(|p| part_has_ajacent_symbol(p, symbols.clone()))
+        .filter(|p| part_has_ajacent_symbol(p, &symbols))
         .map(|p| p.serial_number)
-        .inspect(|x| println!("{}", x))
         .sum()
+}
+
+fn part_02(input: &str) -> u32 {
+    let available_symbols = vec!['*'];
+
+    let parts = input
+        .lines()
+        .enumerate()
+        .flat_map(|(i, line)| parse_digits(i, line))
+        .collect::<Vec<_>>();
+
+    let symbols = input
+        .lines()
+        .enumerate()
+        .fold(vec![], |mut acc: Vec<Symbol>, (i, line)| {
+            acc.append(&mut parse_symbols(i, line, &available_symbols));
+            acc
+        });
+
+    symbols
+        .iter()
+        .map(|s| find_adjacent_parts_for_symbol(s, &parts))
+        .filter(|p| p.len() == 2)
+        .map(|p| p[0].serial_number * p[1].serial_number)
+        .sum()
+}
+
+fn find_adjacent_parts_for_symbol(symbol: &Symbol, parts: &[MachinePart]) -> Vec<MachinePart> {
+    parts
+        .iter()
+        .filter(|p| is_adjacent(p, symbol))
+        .cloned()
+        .collect::<Vec<_>>()
 }
 
 fn parse_digits(y_pos: usize, line: &str) -> Vec<MachinePart> {
@@ -49,12 +81,12 @@ fn parse_digits(y_pos: usize, line: &str) -> Vec<MachinePart> {
     let mut machine_parts: Vec<MachinePart> = Vec::new();
 
     while let Some((i, c)) = iter.next() {
-        if c.is_digit(10) {
+        if c.is_ascii_digit() {
             let x_start = i;
             let x_end = x_start
                 + iter
                     .by_ref()
-                    .take_while(|(_, next_char)| next_char.is_digit(10))
+                    .take_while(|(_, next_char)| next_char.is_ascii_digit())
                     .count();
 
             let number_str: String = line[x_start..=x_end].chars().collect();
@@ -73,7 +105,7 @@ fn parse_digits(y_pos: usize, line: &str) -> Vec<MachinePart> {
     machine_parts
 }
 
-fn parse_symbols(y_pos: usize, input: &str, available_symbols: Vec<char>) -> Vec<Symbol> {
+fn parse_symbols(y_pos: usize, input: &str, available_symbols: &[char]) -> Vec<Symbol> {
     input
         .chars()
         .enumerate()
@@ -82,7 +114,7 @@ fn parse_symbols(y_pos: usize, input: &str, available_symbols: Vec<char>) -> Vec
         .collect::<Vec<_>>()
 }
 
-fn part_has_ajacent_symbol(part: &MachinePart, symbols: Vec<Symbol>) -> bool {
+fn part_has_ajacent_symbol(part: &MachinePart, symbols: &[Symbol]) -> bool {
     symbols.iter().any(|s| is_adjacent(part, s))
 }
 
@@ -134,5 +166,20 @@ mod tests {
     fn larger_test() {
         let input = std::fs::read_to_string("test").unwrap();
         assert_eq!(crate::part_01(&input), 2781);
+    }
+
+    #[test]
+    fn part_02() {
+        let input = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+        assert_eq!(crate::part_02(input), 467835);
     }
 }
