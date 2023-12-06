26c010c3b820c0e2f5c6bb71a5136355aea1dd17
DaAlbrecht <dadidi42@gmail.com>
2023-12-06 01:32:48 +0100

    part_01

--- 2023/day_05/src/main.rs
+++ 2023/day_05/src/main.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 #[derive(Debug)]
 enum Maps {
     Seed2Soil(Vec<GardenMap>),
@@ -11,14 +13,14 @@ enum Maps {
 
 #[derive(Debug)]
 struct Seed {
-    id: u32,
-    soil: Option<u32>,
-    fertilizer: Option<u32>,
-    water: Option<u32>,
-    light: Option<u32>,
-    temperature: Option<u32>,
-    humidity: Option<u32>,
-    location: Option<u32>,
+    id: u64,
+    soil: Option<u64>,
+    fertilizer: Option<u64>,
+    water: Option<u64>,
+    light: Option<u64>,
+    temperature: Option<u64>,
+    humidity: Option<u64>,
+    location: Option<u64>,
 }
 
 impl Default for Seed {
@@ -38,24 +40,28 @@ impl Default for Seed {
 
 #[derive(Debug)]
 struct GardenMap {
-    sources: Vec<u32>,
-    targets: Vec<u32>,
+    source: u64,
+    target: u64,
+    range: u64,
 }
 
 impl From<(u32, u32, u32)> for GardenMap {
     fn from((source, target, range): (u32, u32, u32)) -> Self {
-        let targets = (target..target + range).collect::<Vec<_>>();
-        let sources = (source..source + range).collect::<Vec<_>>();
-        Self { sources, targets }
+        Self {
+            source: source as u64,
+            target: target as u64,
+            range: range as u64,
+        }
     }
 }
 
 fn main() {
     let input = std::fs::read_to_string("puzzle").unwrap();
-    print!("part 01: {}", part_01(&input));
+    //print!("part 01: {}", part_01(&input));
+    print!("part 02: {}", part_02(&input));
 }
 
-fn part_01(input: &str) -> u32 {
+fn part_01(input: &str) -> u64 {
     let mut seeds = parse_seeds(input);
     let maps = parse_maps(input);
     maps.iter().for_each(|map| {
@@ -69,6 +75,63 @@ fn part_01(input: &str) -> u32 {
         .map(|seed| seed.location.unwrap())
         .min()
         .unwrap()
+}
+
+fn part_02(input: &str) -> u64 {
+    let mut seeds = parse_seeds_part02(input);
+    let maps = parse_maps(input);
+    maps.iter().for_each(|map| {
+        seeds.iter_mut().for_each(|seed| {
+            generate_seed_for_map(seed, map);
+        });
+    });
+
+    seeds
+        .iter()
+        .map(|seed| seed.location.unwrap())
+        .max()
+        .unwrap()
+}
+
+fn parse_seeds_part02(input: &str) -> Vec<Seed> {
+    input
+        .lines()
+        .take_while(|line| !line.is_empty())
+        .map(|line| {
+            let chunks = line
+                .strip_prefix("seeds: ")
+                .expect("puzzle to start with 'Seeds: '")
+                .split(' ')
+                .chunks(2);
+
+            let chunks = chunks.into_iter();
+
+            let ids = chunks
+                .map(|chunk| {
+                    let mut chunk = chunk;
+                    let seed = chunk
+                        .next()
+                        .unwrap()
+                        .parse::<u32>()
+                        .expect("seed to be a number");
+                    let range = chunk
+                        .next()
+                        .unwrap()
+                        .parse::<u32>()
+                        .expect("range to be a number");
+                    (seed..seed + range).collect::<Vec<_>>()
+                })
+                .flatten()
+                .collect::<Vec<_>>();
+            ids.iter()
+                .map(|id| Seed {
+                    id: *id as u64,
+                    ..Default::default()
+                })
+                .collect::<Vec<_>>()
+        })
+        .flatten()
+        .collect::<Vec<_>>()
 }
 
 fn parse_maps(input: &str) -> Vec<Maps> {
@@ -165,7 +228,7 @@ fn parse_seeds(input: &str) -> Vec<Seed> {
     seeds
         .iter()
         .map(|seed| Seed {
-            id: *seed,
+            id: *seed as u64,
             ..Default::default()
         })
         .collect::<Vec<_>>()
@@ -175,106 +238,90 @@ fn generate_seed_for_map(seed: &mut Seed, map: &Maps) {
     match map {
         Maps::Seed2Soil(garden_maps) => {
             garden_maps.iter().for_each(|garden_map| {
-                if garden_map.sources.contains(&seed.id) {
-                    let index = garden_map
-                        .sources
-                        .iter()
-                        .position(|&x| x == seed.id)
-                        //seed needs to be in the sources
-                        .unwrap();
-                    seed.soil = Some(garden_map.targets[index]);
-                } else {
-                    seed.soil = Some(seed.id);
+                //check if source is in the range of garden_map.source .. garden_map.source + garden_map.range
+                if garden_map.source <= seed.id && seed.id <= garden_map.source + garden_map.range {
+                    let difference = seed.id - garden_map.source;
+                    seed.soil = Some(garden_map.target as u64 + difference as u64);
+                } else if seed.soil.is_none() {
+                    seed.soil = Some(seed.id as u64);
                 }
             });
         }
         Maps::Soil2Fertilizer(garden_maps) => {
             garden_maps.iter().for_each(|garden_map| {
-                if garden_map.sources.contains(&seed.soil.unwrap()) {
-                    let index = garden_map
-                        .sources
-                        .iter()
-                        .position(|&x| x == seed.soil.unwrap())
-                        //seed needs to be in the sources
-                        .unwrap();
-                    seed.fertilizer = Some(garden_map.targets[index]);
-                } else {
-                    seed.fertilizer = Some(seed.soil.unwrap());
+                //check if source is in the range of garden_map.source .. garden_map.source + garden_map.range
+                if garden_map.source <= seed.soil.unwrap()
+                    && seed.soil.unwrap() <= garden_map.source + garden_map.range
+                {
+                    let difference = seed.soil.unwrap() - garden_map.source;
+                    seed.fertilizer = Some(garden_map.target as u64 + difference as u64);
+                } else if seed.fertilizer.is_none() {
+                    seed.fertilizer = Some(seed.soil.unwrap() as u64);
                 }
             });
         }
         Maps::Fertilizer2Water(garden_maps) => {
             garden_maps.iter().for_each(|garden_map| {
-                if garden_map.sources.contains(&seed.fertilizer.unwrap()) {
-                    let index = garden_map
-                        .sources
-                        .iter()
-                        .position(|&x| x == seed.fertilizer.unwrap())
-                        //seed needs to be in the sources
-                        .unwrap();
-                    seed.water = Some(garden_map.targets[index]);
-                } else {
-                    seed.water = Some(seed.fertilizer.unwrap());
+                //check if source is in the range of garden_map.source .. garden_map.source + garden_map.range
+                if garden_map.source <= seed.fertilizer.unwrap()
+                    && seed.fertilizer.unwrap() <= garden_map.source + garden_map.range
+                {
+                    let difference = seed.fertilizer.unwrap() - garden_map.source;
+                    seed.water = Some(garden_map.target as u64 + difference as u64);
+                } else if seed.water.is_none() {
+                    seed.water = Some(seed.fertilizer.unwrap() as u64);
                 }
             });
         }
         Maps::Water2Light(garden_maps) => {
             garden_maps.iter().for_each(|garden_map| {
-                if garden_map.sources.contains(&seed.water.unwrap()) {
-                    let index = garden_map
-                        .sources
-                        .iter()
-                        .position(|&x| x == seed.water.unwrap())
-                        //seed needs to be in the sources
-                        .unwrap();
-                    seed.light = Some(garden_map.targets[index]);
-                } else {
-                    seed.light = Some(seed.water.unwrap());
+                //check if source is in the range of garden_map.source .. garden_map.source + garden_map.range
+                if garden_map.source <= seed.water.unwrap()
+                    && seed.water.unwrap() <= garden_map.source + garden_map.range
+                {
+                    let difference = seed.water.unwrap() - garden_map.source;
+                    seed.light = Some(garden_map.target as u64 + difference as u64);
+                } else if seed.light.is_none() {
+                    seed.light = Some(seed.water.unwrap() as u64);
                 }
             });
         }
         Maps::Light2Temperature(garden_maps) => {
             garden_maps.iter().for_each(|garden_map| {
-                if garden_map.sources.contains(&seed.light.unwrap()) {
-                    let index = garden_map
-                        .sources
-                        .iter()
-                        .position(|&x| x == seed.light.unwrap())
-                        //seed needs to be in the sources
-                        .unwrap();
-                    seed.temperature = Some(garden_map.targets[index]);
-                } else {
-                    seed.temperature = Some(seed.light.unwrap());
+                //check if source is in the range of garden_map.source .. garden_map.source + garden_map.range
+                if garden_map.source <= seed.light.unwrap()
+                    && seed.light.unwrap() <= garden_map.source + garden_map.range
+                {
+                    let difference = seed.light.unwrap() - garden_map.source;
+                    seed.temperature = Some(garden_map.target as u64 + difference as u64);
+                } else if seed.temperature.is_none() {
+                    seed.temperature = Some(seed.light.unwrap() as u64);
                 }
             });
         }
         Maps::Temperature2Humidity(garden_maps) => {
             garden_maps.iter().for_each(|garden_map| {
-                if garden_map.sources.contains(&seed.temperature.unwrap()) {
-                    let index = garden_map
-                        .sources
-                        .iter()
-                        .position(|&x| x == seed.temperature.unwrap())
-                        //seed needs to be in the sources
-                        .unwrap();
-                    seed.humidity = Some(garden_map.targets[index]);
-                } else {
-                    seed.humidity = Some(seed.temperature.unwrap());
+                //check if source is in the range of garden_map.source .. garden_map.source + garden_map.range
+                if garden_map.source <= seed.temperature.unwrap()
+                    && seed.temperature.unwrap() <= garden_map.source + garden_map.range
+                {
+                    let difference = seed.temperature.unwrap() - garden_map.source;
+                    seed.humidity = Some(garden_map.target as u64 + difference as u64);
+                } else if seed.humidity.is_none() {
+                    seed.humidity = Some(seed.temperature.unwrap() as u64);
                 }
             });
         }
         Maps::Humidity2Location(garden_maps) => {
             garden_maps.iter().for_each(|garden_map| {
-                if garden_map.sources.contains(&seed.humidity.unwrap()) {
-                    let index = garden_map
-                        .sources
-                        .iter()
-                        .position(|&x| x == seed.humidity.unwrap())
-                        //seed needs to be in the sources
-                        .unwrap();
-                    seed.location = Some(garden_map.targets[index]);
-                } else {
-                    seed.location = Some(seed.humidity.unwrap());
+                //check if source is in the range of garden_map.source .. garden_map.source + garden_map.range
+                if garden_map.source <= seed.humidity.unwrap()
+                    && seed.humidity.unwrap() <= garden_map.source + garden_map.range
+                {
+                    let difference = seed.humidity.unwrap() - garden_map.source;
+                    seed.location = Some(garden_map.target as u64 + difference as u64);
+                } else if seed.location.is_none() {
+                    seed.location = Some(seed.humidity.unwrap() as u64);
                 }
             });
         }
@@ -288,5 +335,10 @@ mod tests {
     fn part_01() {
         let input = std::fs::read_to_string("test").unwrap();
         assert_eq!(crate::part_01(&input), 35);
+    }
+    #[test]
+    fn part_02() {
+        let input = std::fs::read_to_string("test").unwrap();
+        assert_eq!(crate::part_02(&input), 46);
     }
 }
