e0eee8a3f90cb434b97d61d8ebeb1047062dd367
DaAlbrecht <dadidi42@gmail.com>
2024-12-08 23:35:41 +0100

    feat: day_08 part 2

--- 2024/day_08/src/main.rs
+++ 2024/day_08/src/main.rs
@@ -5,9 +5,14 @@ use grid::Grid;
 fn main() {
     let input = std::fs::read_to_string("input").unwrap();
     println!("part_01: {}", part_01(input.as_str()));
+    println!("part_02: {}", part_02(input.as_str()));
 }
 
 fn part_01(input: &str) -> usize {
+    process(input, false)
+}
+
+fn process(input: &str, calc_antinode_many: bool) -> usize {
     let mut antennas: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
     let mut antinodes = HashSet::new();
     let col_lenght = input.lines().next().unwrap().chars().count();
@@ -35,9 +40,19 @@ fn part_01(input: &str) -> usize {
         for a in antennas.get(key).unwrap() {
             for b in antennas.get(key).unwrap() {
                 if a != b {
-                    let possible_antinode = calc_antinode(a, b);
-                    if grid.get(possible_antinode.0, possible_antinode.1).is_some() {
-                        antinodes.insert(possible_antinode);
+                    if calc_antinode_many {
+                        let many_possible_antinodes =
+                            calc_antinode_part_2(a, b, grid.rows(), grid.cols());
+                        for possible_antinode in many_possible_antinodes {
+                            if grid.get(possible_antinode.0, possible_antinode.1).is_some() {
+                                antinodes.insert(possible_antinode);
+                            }
+                        }
+                    } else {
+                        let possible_antinode = calc_antinode(a, b);
+                        if grid.get(possible_antinode.0, possible_antinode.1).is_some() {
+                            antinodes.insert(possible_antinode);
+                        }
                     }
                 }
             }
@@ -45,6 +60,10 @@ fn part_01(input: &str) -> usize {
     }
 
     antinodes.len()
+}
+
+fn part_02(input: &str) -> usize {
+    process(input, true)
 }
 
 #[allow(clippy::cast_possible_wrap)]
@@ -57,6 +76,37 @@ fn calc_antinode(a: &(usize, usize), b: &(usize, usize)) -> (isize, isize) {
     let cx = 2 * bx - ax;
     let cy = 2 * by - ay;
     (cx, cy)
+}
+
+#[allow(clippy::cast_possible_wrap)]
+fn calc_antinode_part_2(
+    a: &(usize, usize),
+    b: &(usize, usize),
+    row_size: usize,
+    col_size: usize,
+) -> Vec<(isize, isize)> {
+    let mut antinodes = Vec::new();
+
+    let ax = a.0 as isize;
+    let ay = a.1 as isize;
+    let bx = b.0 as isize;
+    let by = b.1 as isize;
+
+    let dx = bx - ax;
+    let dy = by - ay;
+
+    for x in 1..=row_size as isize * col_size as isize {
+        let cx = bx + x * dx;
+        let cy = by + x * dy;
+
+        let cx_reverse = bx - x * dx;
+        let cy_reverse = by - x * dy;
+
+        antinodes.push((cx, cy));
+        antinodes.push((cx_reverse, cy_reverse));
+    }
+
+    antinodes
 }
 
 #[cfg(test)]
@@ -76,5 +126,22 @@ mod tests {
 ............
 ............";
         assert_eq!(super::part_01(test_input), 14);
+    }
+
+    #[test]
+    fn part_02() {
+        let test_input = "............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............";
+        assert_eq!(super::part_02(test_input), 34);
     }
 }
