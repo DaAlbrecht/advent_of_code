d7761bd7f6b695c53de629230c164bb599343b51
DaAlbrecht <dadidi42@gmail.com>
2025-12-01 19:45:06 +0100

    feat: day_01, part 2

--- 2025/day_01/src/main.rs
+++ 2025/day_01/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let file = std::fs::read_to_string("input").unwrap();
     dbg!(part_01(&file));
+    dbg!(part_02(&file));
 }
 
 fn part_01(file: &str) -> usize {
@@ -22,9 +23,41 @@ fn part_01(file: &str) -> usize {
     count_zero
 }
 
+fn part_02(file: &str) -> usize {
+    let mut dial: i32 = 50;
+    let mut count_zero = 0;
+
+    for line in file.lines() {
+        let (direction, number) = line.split_at(1);
+        let number = number.parse::<i32>().unwrap();
+
+        match direction {
+            "R" => {
+                for i in 1..=number {
+                    if (dial + i).rem_euclid(100) == 0 {
+                        count_zero += 1;
+                    }
+                }
+                dial = (dial + number).rem_euclid(100);
+            }
+            "L" => {
+                for i in 1..=number {
+                    if (dial - i).rem_euclid(100) == 0 {
+                        count_zero += 1;
+                    }
+                }
+                dial = (dial - number).rem_euclid(100);
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    count_zero
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::part_01;
+    use crate::{part_01, part_02};
 
     #[test]
     fn test_01() {
@@ -39,5 +72,20 @@ L99
 R14
 L82";
         assert_eq!(3, part_01(input));
+    }
+
+    #[test]
+    fn test_02() {
+        let input = "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
+        assert_eq!(6, part_02(input));
     }
 }
