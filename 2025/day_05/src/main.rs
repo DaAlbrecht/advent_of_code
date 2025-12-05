e42f26c30f415c28dad02ccb92bef5885b45cfc1
DaAlbrecht <dadidi42@gmail.com>
2025-12-05 16:03:06 +0100

    feat: day_05 part 02

--- 2025/day_05/src/main.rs
+++ 2025/day_05/src/main.rs
@@ -3,6 +3,7 @@ use std::collections::HashSet;
 fn main() {
     let file = std::fs::read_to_string("input").unwrap();
     dbg!(part_01(&file));
+    dbg!(part_02(&file));
 }
 
 fn part_01(input: &str) -> usize {
@@ -36,9 +37,36 @@ fn part_01(input: &str) -> usize {
         .count()
 }
 
+fn part_02(input: &str) -> usize {
+    let (id_ranges, _) = input.split_once("\n\n").unwrap();
+    let mut ranges = Vec::new();
+
+    for line in id_ranges.lines() {
+        let (from, to) = line.split_once('-').unwrap();
+        let from = from.parse::<usize>().unwrap();
+        let to = to.parse::<usize>().unwrap();
+        ranges.push((from, to));
+    }
+
+    ranges.sort_by_key(|r| r.0);
+
+    let mut merged: Vec<(usize, usize)> = Vec::new();
+
+    for (start, end) in ranges {
+        match merged.last_mut() {
+            Some((_, prev_end)) if start <= *prev_end + 1 => {
+                *prev_end = (*prev_end).max(end);
+            }
+            _ => merged.push((start, end)),
+        }
+    }
+
+    merged.into_iter().map(|(from, to)| to - from + 1).sum()
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::part_01;
+    use crate::{part_01, part_02};
 
     #[test]
     fn test_01() {
@@ -55,5 +83,22 @@ mod tests {
 32
 ";
         assert_eq!(3, part_01(input));
+    }
+
+    #[test]
+    fn test_02() {
+        let input = "3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32
+";
+        assert_eq!(14, part_02(input));
     }
 }
